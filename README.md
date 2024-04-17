031c28176b0a7f31b422e463b91688910b1f65a5
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-04-17 10:13:08 +0300

    add queue.py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Data-and-software
+
 # Data-Science
+Tuples:Are variables used to store multiple items in a variable
+
+Stack: It is a linear structure that follows a particular order in which the operaions are perfomed.
---
+++ queue.py
@@ -0,0 +1,14 @@
+# How to crete a queue
+from collections import deque
+queue = []
+queue.append('oranges')
+queue.append('mangoes')
+queue.append('pawpaw')
+queue.append('Guava')
+print(queue)
+
+# remove items in a queue
+queue.pop()
+print(queue)
+
+print(deque)
